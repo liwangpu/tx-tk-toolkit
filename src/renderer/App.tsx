2ca9a0c53e146546abbfe641f0e4df64cd3c14f1
liwang.pu@gmail.com <liwang.pu@gmail.com>
2024-10-18 13:33:12 +0800

    111

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,49 +1,10 @@
 import { observer } from 'mobx-react-lite';
-import { connectReduxDevtools } from 'mst-middlewares';
-import { useEffect } from 'react';
-import { Outlet } from 'react-router-dom';
-import AppSidebar from './components/AppSidebar';
-import styles from './app.module.less';
-import { appStore } from './stores';
-import { ConfigProvider, theme } from 'antd';
 
-const handleDetectStore = () => {
-  connectReduxDevtools(require("remotedev"), appStore);
-};
-
-const themeConfig = {
-  token: {
-    // colorPrimary: 'red',
-    borderRadius: 4,
-  },
-  // algorithm: [theme.compactAlgorithm],
-};
 
 const App = observer(() => {
 
-  // const { TKAccountStore, platformStore, supplierStore, warehouseStore, stationStore } = appStore;
-
-
-  useEffect(() => {
-    // TKAccountStore.getAllShopOptions();
-    // supplierStore.getAllSupplierOptions();
-    // warehouseStore.getAllWarehouseOptions();
-    // platformStore.getAllOptions();
-    // stationStore.getAllOptions();
-    // handleDetectStore();
-  }, []);
-
   return (
-    <ConfigProvider theme={themeConfig}>
-      <div className={styles['app']}>
-        <div className={styles['app__navigation']}>
-          <AppSidebar />
-        </div>
-        <div className={styles['app__page']}>
-          <Outlet />
-        </div>
-      </div>
-    </ConfigProvider>
+    <div>Root</div>
   );
 });
 
--- src/renderer/contexts/app-store.ts
+++
@@ -1,4 +0,0 @@
-// import React from 'react';
-// import { appStore, IAppStore } from '../stores';
-
-// export const AppStoreContext = React.createContext<IAppStore>(appStore);
